5aa67ec9dde162169b57dde12f9df03b4ae2f82f
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-04 14:13:22 -0500

    Add project files.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime = true; 
+
+            for(int i = 2; i < 100_000; i++)
+            {
+
+                for(int x = 2; x < i; x++) //loop that looks to see if there are any factors
+                {
+                    if(i%x == 0)
+                    {
+                        isPrime = false;
+                        x = i;//exits loop once number is proven not to be prime
+                    }
+                }
+
+                if(isPrime) //only print if number did not fail tests
+                {
+                    WriteLine(i);
+                }
+                isPrime = true;
+
+            }
+            
+        }
+    }
+}
